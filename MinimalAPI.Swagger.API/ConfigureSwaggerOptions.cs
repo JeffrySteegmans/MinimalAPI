8a3f368a97b78179a8dabe77d8bb0f725352cb24
Jeffry Steegmans <jeffrysteegmans@gmail.com>
2022-07-11 21:30:42 +0200

    File scoped namespace

--- MinimalAPI.Swagger.API/ConfigureSwaggerOptions.cs
+++ MinimalAPI.Swagger.API/ConfigureSwaggerOptions.cs
@@ -5,40 +5,39 @@ using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System.Text;
 
-namespace MinimalAPI.Swagger.API
+namespace MinimalAPI.Swagger.API;
+
+public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
 {
-    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    private readonly IApiVersionDescriptionProvider provider;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ConfigureSwaggerOptions"/> class.
+    /// </summary>
+    /// <param name="provider">The <see cref="IApiVersionDescriptionProvider">provider</see> used to generate Swagger documents.</param>
+    public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) => this.provider = provider;
+
+    public void Configure(SwaggerGenOptions options)
     {
-        private readonly IApiVersionDescriptionProvider provider;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ConfigureSwaggerOptions"/> class.
-        /// </summary>
-        /// <param name="provider">The <see cref="IApiVersionDescriptionProvider">provider</see> used to generate Swagger documents.</param>
-        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) => this.provider = provider;
-
-        public void Configure(SwaggerGenOptions options)
+        // add a swagger document for each discovered API version
+        // note: you might choose to skip or document deprecated API versions differently
+        foreach (var description in provider.ApiVersionDescriptions)
         {
-            // add a swagger document for each discovered API version
-            // note: you might choose to skip or document deprecated API versions differently
-            foreach (var description in provider.ApiVersionDescriptions)
-            {
-                options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
-            }
+            options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
         }
+    }
 
-        private static OpenApiInfo CreateInfoForApiVersion(ApiVersionDescription description)
+    private static OpenApiInfo CreateInfoForApiVersion(ApiVersionDescription description)
+    {
+        var text = new StringBuilder("An example application with OpenAPI, Swashbuckle, and API versioning.");
+        var info = new OpenApiInfo()
         {
-            var text = new StringBuilder("An example application with OpenAPI, Swashbuckle, and API versioning.");
-            var info = new OpenApiInfo()
-            {
-                Title = "Example API",
-                Version = description.ApiVersion.ToString()
-            };
+            Title = "Example API",
+            Version = description.ApiVersion.ToString()
+        };
 
-            info.Description = text.ToString();
+        info.Description = text.ToString();
 
-            return info;
-        }
+        return info;
     }
 }
